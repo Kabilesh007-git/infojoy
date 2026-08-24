881d52123d113e4367fb2058ad48c835c1c56915
kabilesh007 <kabileshs.mpc.b2@gmail.com>
2026-08-24 21:30:07 +0530

    add/fvaicon

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    message="welcome to the page"
 
 @app.route("/downdload" , methods=["POST"])
 def downdload():
     data=request.form["movie"]
-    return data
+
+    if len(data)==0:
+        return "<h1>enter the name</h1> "
+
+    with open("moviename.txt","w")as txt:
+        txt.write(data)
+
+    return render_template("index1.html")
+
 
 
 if __name__=="__main__":
